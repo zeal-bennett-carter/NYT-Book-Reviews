5aa9feffe6f097584e47676e19517f70336ace2c
Bennett Carter <bennett.carter@zealitconsultants.com>
2024-08-22 14:49:31 -0500

    adjusted capitalization and styling

--- nyt-book-reviews/src/components/bookslist.tsx
+++ nyt-book-reviews/src/components/bookslist.tsx
@@ -4,6 +4,14 @@ import { BookListing, retrieveBooksList } from "../services/bookslistretriever";
 export default function BooksList() {
     const [booksList, setBooksList] = useState<BookListing[]>([]);
 
+    const capitalizeWords = (str: string): string => {
+        return str
+            .toLowerCase()
+            .split(' ')
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(' ');
+    };
+
     useEffect(() => {
         const getBooksList = async () => {
             try {
@@ -19,25 +27,23 @@ export default function BooksList() {
     }, [])
 
     return (
-            <table>
-            <thead>
+        <table className="books-table">
+            <thead className="books-table-title">
                 <tr>
-                    <th></th>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Publisher</th>
-                    <th>Price</th>
+                    <th className="column-header">Title</th>
+                    <th className="column-header">Author</th>
+                    <th className="column-header">Publisher</th>
+                    <th className="column-header">Price</th>
                 </tr>
             </thead>
             <tbody>
                 {
                     booksList.map((book, index) => (
-                        <tr key={index}>
-                            <td>{index}</td>
-                            <td>{book.title}</td>
-                            <td>{book.author}</td>
-                            <td>{book.publisher}</td>
-                            <td>{book.price}</td>
+                        <tr className="book-table-row" key={index}>
+                            <td className="book-table-cell book-title">{book.title}</td>
+                            <td className="book-table-cell">{capitalizeWords(book.author)}</td>
+                            <td className="book-table-cell">{book.publisher}</td>
+                            <td className="book-table-cell book-price">${book.price}</td>
                         </tr>
                     ))
                 }
